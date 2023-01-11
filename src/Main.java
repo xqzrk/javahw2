d243aafa1869c0277b3dc97be02507da85124f19
MSevryukov <smlog@yandex.ru>
2023-01-12 00:12:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 2400;
+        int bonus = 20;
+        int bonusMiles = price / bonus;
+        System.out.println("Накоплено бонусных миль - " + bonusMiles);
+    }
+}
+
+
+
